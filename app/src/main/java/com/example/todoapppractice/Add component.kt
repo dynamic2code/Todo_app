ad421f3aa594f0177cf659ce1e08b57b75f3b00b
dynamic2code <dynamicdesingns@gmail.com>
2023-04-27 13:36:33 +0300

    error point on Add component

--- app/src/main/java/com/example/todoapppractice/Add component.kt
+++ app/src/main/java/com/example/todoapppractice/Add component.kt
@@ -1,8 +1,31 @@
 package com.example.todoapppractice
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.todoapppractice.ui.theme.TodoApppracticeTheme
 
 @Composable
 fun Add(){
-
+    Icon(Icons.Filled.Add,"")
+}
+@Preview(showBackground = true)
+@Composable
+fun AddPreview() {
+    TodoApppracticeTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background
+        ) {
+            Add()
+        }
+    }
 }
